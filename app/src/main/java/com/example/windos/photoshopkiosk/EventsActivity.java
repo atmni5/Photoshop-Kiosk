044bfa02cf072be257ceb9e37edb190d79bcf5c0
atmni5 <aaron.tumini@griffithuni.edu.au>
2017-03-12 20:02:40 +1000

    Added Year activity view

--- app/src/main/java/com/example/windos/photoshopkiosk/EventsActivity.java
+++ app/src/main/java/com/example/windos/photoshopkiosk/EventsActivity.java
@@ -1,7 +1,10 @@
 package com.example.windos.photoshopkiosk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EventsActivity extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class EventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
     }
+
+    public void onButtonClick(View v){
+        Button button2 = (Button) v;
+        startActivity(new Intent(getApplicationContext(), nextScreen.class));
+    }
+
 }
