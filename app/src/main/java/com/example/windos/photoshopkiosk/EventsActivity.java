5a251c72e161029f2186d145bd23024986e359fd
atmni5 <aaron.tumini@griffithuni.edu.au>
2017-03-14 19:36:20 +1000

    Event buttons

--- app/src/main/java/com/example/windos/photoshopkiosk/EventsActivity.java
+++ app/src/main/java/com/example/windos/photoshopkiosk/EventsActivity.java
@@ -3,20 +3,23 @@ package com.example.windos.photoshopkiosk;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class EventsActivity extends AppCompatActivity {
-
+    public static final String TAG = "Photoshop Kiosk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
     }
 
-    public void onButtonClick(View v){
-        Button button2 = (Button) v;
-        startActivity(new Intent(getApplicationContext(), nextScreen.class));
+    public void onEventClick(View v){
+        ImageButton event = (ImageButton) v;
+        Log.d(TAG, "Hello " + getResources().getResourceEntryName(event.getId()) + " Yo Whaddup");
+        startActivity(new Intent(getApplicationContext(), YearActivity.class));
     }
 
 }
--- app/src/main/java/com/example/windos/photoshopkiosk/YearActivity.java
+++ app/src/main/java/com/example/windos/photoshopkiosk/YearActivity.java
@@ -2,12 +2,14 @@ package com.example.windos.photoshopkiosk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class YearActivity extends AppCompatActivity {
-
+    public static final String TAG = "Photoshop Kiosk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_year);
     }
+
 }
